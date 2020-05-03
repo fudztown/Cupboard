638c49985e9497d11c64e8c8408b7e8f8bde5538
Pete Town <fudz.town@gmail.com>
2020-05-03 16:38:32 +0100

    Initial commit

--- app/src/main/java/com/asktown/cupboard/data/model/LoggedInUser.kt
+++ app/src/main/java/com/asktown/cupboard/data/model/LoggedInUser.kt
@@ -4,6 +4,10 @@ package com.asktown.cupboard.data.model
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
 data class LoggedInUser(
-    val userId: String,
-    val displayName: String
+    val user_id: String,
+    val displayName: String,
+    var email: String,
+    var security_level: String,
+    var profile_image: String,
+    var phone: String
 )
