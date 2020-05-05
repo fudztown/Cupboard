d9a60bebd5c6cc17aa9edf3a5ad23d313297564f
fudz.town@gmail.com <fudz.town@gmail.com>
2020-05-05 10:22:41 +0100

    to git hub?

--- app/src/main/java/com/asktown/cupboard/data/LoginDataSource.kt
+++ app/src/main/java/com/asktown/cupboard/data/LoginDataSource.kt
@@ -14,7 +14,7 @@ class LoginDataSource {
             val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), username)
             return Result.Success(fakeUser)
         } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+            return Result.Error(IOException("Error logging in 123", e))
         }
     }
 
